e28a56b780e4fb5b7058883bd27debccec66b32c
Momen El Kamri <momen.elkamri@ns.nl>
2024-01-19 20:53:17 +0100

    add orange color

--- app/views/home/home.php
+++ app/views/home/home.php
@@ -10,12 +10,12 @@
 
 <main>
     <section class="container mb-5 mt-5">
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit</h2>
+        <h2><span class="fa-text-orange">Lorem ipsum dolor</span> sit amet consectetur adipisicing elit</h2>
         <p>Lorem ipsum dolor  sit amet consectetu  adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
     </section>
 
     <section class="container mb-5 mt-5 reveal">
-        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
+        <h3><span class="fa-text-orange">Lorem ipsum dolor sit</span> amet consectetur adipisicing elit</h3>
         <img class="width-75" src="https://uploads-ssl.webflow.com/611b83ce6109d4cce21d5d39/611bb1326f7c3cc69f88afcc_Riesen_11_B_V5.jpg" loading="lazy">
         <p class="my-4">Lorem ipsum dolor  sit amet consectetu  adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
         <p class="my-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde inventore id eaque provident beatae enim explicabo rem porro iure, vel molestias voluptate, quae qui earum illo fugiat! Illo, provident sapiente?</p>
@@ -23,13 +23,13 @@
 
     <section class="fa-why-wrapper reveal">
         <div class="container">
-            <h2 class="text-light">Lorem ipsum dolor sit</h2>
+            <h2>Lorem ipsum dolor sit</h2>
             <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio modi reprehenderit non magni consequuntur unde, sint nesciunt explicabo perspiciatis sunt id veniam odio consectetur quia. Temporibus numquam accusamus voluptatem nobis.</p>
         </div>
     </section>
 
     <section class="container mb-5 mt-5 reveal">
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit</h2>
+        <h2><span class="fa-text-orange">Lorem ipsum dolor sit</span> amet consectetur adipisicing elit</h2>
         <p class="my-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde inventore id eaque provident beatae enim explicabo rem porro iure, vel molestias voluptate, quae qui earum illo fugiat! Illo, provident sapiente?</p>
     </section>
 
