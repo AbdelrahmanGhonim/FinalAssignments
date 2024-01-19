6f44a023ec0286e9ef84893221cfb6cf4f8e953b
Momen El Kamri <momen.elkamri@ns.nl>
2024-01-19 23:13:34 +0100

    add image and text to progressbar page

--- app/views/ProgressTracker.php
+++ app/views/ProgressTracker.php
@@ -7,73 +7,46 @@
 <main class="container">
     <h1>Progress Tracker</h1>
     <?php if (isset($SESSION_user_name)) { ?>
-    <div>
-        <section class="card fa-wide-card">
-            <div class="fa-card-body">
-                <h2 class="card-title">Calories</h2>
-                <div class="fa-session-info">
-                    <p class="session-goal">
-                        Goal: <?php echo $_SESSION['goal']; ?>
-                    </p>
-                    <p class="session-weight">
-                        Weight: <?php echo $_SESSION['weight']; ?>
-                    </p>
-                </div>
-                <p class="fa-card-text">
-                    Remaining = Goal - Food + Exercise
+    <section class="card fa-wide-card">
+        <div class="fa-card-body">
+            <h2 class="card-title">Calories</h2>
+            <div class="fa-session-info">
+                <p class="session-goal">
+                    Goal: <?php echo $_SESSION['goal']; ?>
                 </p>
-                <div class="fa-circle">
-                    <p class="fa-caloriesIntake">
-                        <?php echo $_SESSION['caloriesIntake']; ?>
-                    </p>
-                </div>
+                <p class="session-weight">
+                    Weight: <?php echo $_SESSION['weight']; ?>
+                </p>
             </div>
-        </section>
-        <p>* This calculation based on 100g per each</p>
-        <table class="table" id="nutritionTable">
-            <thead>
-                <tr>
-                </tr>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
-    </div>
-    <? } ?>
+            <p class="fa-card-text">
+                Remaining = Goal - Food + Exercise
+            </p>
+            <div class="fa-circle">
+                <p class="fa-caloriesIntake">
+                    <?php echo $_SESSION['caloriesIntake']; ?>
+                </p>
+            </div>
+        </div>
+    </section>
+    <p>* This calculation based on 100g per each</p>
+    <table class="table" id="nutritionTable">
+        <thead>
+            <tr>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+    <?php } else { ?>
+    <section class="card fa-wide-card p-0">
+        <img src="https://uploads-ssl.webflow.com/612c9fe71d22d5586d8b2291/626d18cdabfcc7bf43e2a07b_Training%20als%20Anfa%CC%88nger.jpg" />
+        <div class="fa-wide-card-content">
+            <h2>Calories</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates vel magnam iste alias quaerat soluta fuga error facilis facere quo rem adipisci, odio saepe recusandae exercitationem distinctio aperiam laborum. Debitis?</p>
+        </div>
+    </section>
 
-    
-    <div>
-        <section class="card fa-wide-card">
-            <div class="fa-card-body">
-                <h2 class="card-title">Calories</h2>
-                <div class="fa-session-info">
-                    <p class="session-goal">
-                        Goal: <?php echo $_SESSION['goal']; ?>
-                    </p>
-                    <p class="session-weight">
-                        Weight: <?php echo $_SESSION['weight']; ?>
-                    </p>
-                </div>
-                <p class="fa-card-text">
-                    Remaining = Goal - Food + Exercise
-                </p>
-                <div class="fa-circle">
-                    <p class="fa-caloriesIntake">
-                        <?php echo $_SESSION['caloriesIntake']; ?>
-                    </p>
-                </div>
-            </div>
-        </section>
-        <p>* This calculation based on 100g per each</p>
-        <table class="table" id="nutritionTable">
-            <thead>
-                <tr>
-                </tr>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
-    </div>
+    <?php } ?>
 </main>
 
 
