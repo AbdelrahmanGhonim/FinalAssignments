e4d31c5fa624459612347ccd3640575a4615e74a
Momen El Kamri <momen.elkamri@ns.nl>
2024-01-20 15:26:15 +0100

    login page style

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -9,19 +9,27 @@
 </header>
 
 <main class="container">
-    <form action="/login/authenticate" method="POST">
-        <div class="mb-3">
-            <label for="username" class="form-label">Username:</label>
-            <input type="text" class="form-control" id="username" name="username" required>
+    <div class="row">
+        <div class="col-md-7">
+            <h2><span class="fa-text-orange">Lorem ipsum dolor</span> sit amet consectetur adipisicing elit</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque natus blanditiis dolores officiis minus, ut corporis distinctio. Officiis nam consectetur reiciendis possimus harum nobis nisi. Officiis nesciunt officia aut totam?</p>
         </div>
-
-        <div class="mb-3">
-            <label for="password" class="form-label">Password:</label>
-            <input type="password" class="form-control" id="password" name="password" required>
+        <div class="col-md-5">
+            <form action="/login/authenticate" method="POST">
+                <div class="mb-3">
+                    <label for="username" class="form-label">Username:</label>
+                    <input type="text" class="form-control" id="username" name="username" required>
+                </div>
+        
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password:</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
+                </div>
+        
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
         </div>
-
-        <button type="submit" class="btn btn-primary">Login</button>
-    </form>
+    </div>
 </main>
 
 <?php
